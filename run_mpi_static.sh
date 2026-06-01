bb4c13ebe7f9eae9198e1ccaa0be77a968417c32
Andy3325 <zhum60517@gmail.com>
2026-06-01 23:02:14 +0800

    Add MPI static cyclic baseline for password guessing

--- run_mpi_static.sh
+++ run_mpi_static.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-set -e
+#PBS -N mpi_static
+#PBS -e mpi_static.e
+#PBS -o mpi_static.o
+#PBS -l nodes=1:ppn=8
 
-mpicxx -O2 -std=c++17 correctness_guess_mpi_static.cpp train.cpp guessing.cpp md5.cpp -o mpi_static
+MASTER_DIR=/home/${USER}/guess
+WORK_DIR=/home/${USER}/mpi_static_work
+EXE=mpi_static
+NP=8
 
-mpiexec -n 1 ./mpi_static
-mpiexec -n 2 ./mpi_static
-mpiexec -n 4 ./mpi_static
-mpiexec -n 8 ./mpi_static
+NODES=$(cat $PBS_NODEFILE | sort | uniq)
+
+for node in $NODES; do
+    ssh ${node} "mkdir -p ${WORK_DIR}" 1>&2
+    scp master_ubss1:${MASTER_DIR}/${EXE} ${node}:${WORK_DIR}/${EXE} 1>&2
+done
+
+cd ${WORK_DIR} || exit 1
+
+/usr/local/bin/mpiexec -np ${NP} -machinefile $PBS_NODEFILE ${WORK_DIR}/${EXE}
